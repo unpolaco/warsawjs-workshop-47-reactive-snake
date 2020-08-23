17839178cd50a1a2a3456c97660f38ba7ff9fb92
Michal Borowski <unpolacoo@gmail.com>
2020-08-23 11:49:54 +0200

    Create snake and random fruit position

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import GameGrid from './game/GameGrid.jsx'
+import React, { useState } from 'react';
+import GameGrid from './game/GameGrid.jsx';
 
 const GridSize = 10;
 
 function App() {
-  return (
-    <GameGrid gridSize={GridSize}/>
-  );
+	const [snake, setSnake] = useState([
+		{ x: GridSize / 2, y: GridSize / 2 },
+		{ x: GridSize / 2, y: GridSize / 2 + 1 },
+		{ x: GridSize / 2, y: GridSize / 2 + 2 },
+	]);
+	const [fruit, setFruit] = useState({
+		x: randomIndex(GridSize),
+		y: randomIndex(GridSize),
+	});
+
+	function randomIndex(n) {
+		return Math.floor(Math.random() * n);
+	}
+
+	return <GameGrid gridSize={GridSize} fruit={fruit} snake={snake} />;
 }
 
 export default App;
