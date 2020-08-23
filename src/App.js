6848173b2969573d8a58cea0af3923d57a25f3fe
Michal Borowski <unpolacoo@gmail.com>
2020-08-24 00:04:29 +0200

    Create useGame custom hook

--- src/App.js
+++ src/App.js
@@ -1,81 +1,17 @@
-import React, { useState, useEffect, useRef } from 'react';
-import GameGrid from './game/GameGrid.jsx';
-
-const GridSize = 10;
+import React, { useState } from 'react';
+import useGame from './game/useGame'
+import GameGrid from './game/GameGrid';
 
 function App() {
-	const [snake, setSnake] = useState([
-		{ x: GridSize / 2, y: GridSize / 2 },
-		{ x: GridSize / 2, y: GridSize / 2 + 1 },
-		{ x: GridSize / 2, y: GridSize / 2 + 2 },
-	]);
-	const [fruit, setFruit] = useState({
-		x: randomIndex(GridSize),
-		y: randomIndex(GridSize),
-  });
-  const [speed, setSpeed] = useState(500);
-
-	const direction = useRef('up');
+  
+  const GridSize = 10;
   const SpeedMultiplier = 0.8;
+  const [speed, setSpeed] = useState(500);
+  const { fruit, snake } = useGame({ speed, gridSize: GridSize, increaseSpeed })
 
-  
-	useEffect(() => {
-		const interval = setInterval(() => {
-			const [snakeHead] = snake;
-      const newSnakeHead = { ...snakeHead };
-      const fruitEaten = fruit.x === newSnakeHead.x && fruit.y === newSnakeHead.y;
-
-      if (fruitEaten) {
-        setFruit({
-          x: randomIndex(GridSize),
-          y: randomIndex(GridSize),
-        })
-        setSpeed(s => s * SpeedMultiplier)
-      }
-
-      if (direction.current === 'up') {
-        newSnakeHead.x -= 1;
-      } else if (direction.current === 'down') {
-        newSnakeHead.x += 1;
-      } else if (direction.current === 'right') {
-        newSnakeHead.y += 1;
-      } else if (direction.current === 'left') {
-        newSnakeHead.y -= 1;
-      }
-      const newSnake = fruitEaten 
-      ? [newSnakeHead, ...snake.slice(0, snake.length)] 
-      : [newSnakeHead, ...snake.slice(0, snake.length - 1)] ;
-      setSnake(newSnake);
-    }, speed);
-
-		return () => {
-			clearInterval(interval);
-		};
-  }, [snake]);
-  
-  useEffect(() => {
-    document.addEventListener('keydown', handleKeyDown);
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown);
-    };
-    }, []);
-
-    function handleKeyDown(e) {
-      console.log(e.key);
-      if(e.key === 'ArrowUp') {
-        direction.current = 'up'
-      } else if(e.key === 'ArrowDown') {
-        direction.current = 'down'
-      } else if(e.key === 'ArrowLeft') {
-        direction.current = 'left'
-      } else if(e.key === 'ArrowRight') {
-        direction.current = 'right'
-      }
-    }
-
-	function randomIndex(n) {
-		return Math.floor(Math.random() * n);
-	}
+  function increaseSpeed() {
+    setSpeed( s => s * SpeedMultiplier)
+  }
 
 	return <GameGrid gridSize={GridSize} fruit={fruit} snake={snake} />;
 }
