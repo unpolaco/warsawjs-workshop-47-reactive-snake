9c40a9535472eaf4a4c8b3b31e540fd42eadec70
Michal Borowski <unpolacoo@gmail.com>
2020-08-24 21:57:22 +0200

    Add pause/unpause context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import useGame from './game/useGame';
 import Game from './game/Game';
 import GameContext from './game/GameContext';
 
@@ -7,14 +6,16 @@ function App() {
 	const gridSize = 10;
 	const SpeedMultiplier = 0.8;
 	const [speed, setSpeed] = useState(500);
-	// const { fruit, snake } = useGame({
-	// 	speed,
-	// 	gridSize: gridSize,
-	// 	increaseSpeed,
-	// });
+	const [paused, setPaused] = useState(true);
 
 	function increaseSpeed() {
 		setSpeed((s) => s * SpeedMultiplier);
+	}
+	function pauseGame() {
+		setPaused(true);
+	}
+	function unpauseGame() {
+		setPaused(false);
 	}
 
 	return (
--- src/game/GameContext.js
+++ src/game/GameContext.js
@@ -1,5 +1,12 @@
 import { createContext } from 'react';
 
-const GameContext = createContext({ gridSize: 0, speed: 0, increaseSpeed: () => {} });
+const GameContext = createContext({
+	gridSize: 0,
+	speed: 0,
+	paused: true,
+	increaseSpeed: () => {},
+	pauseGame: () => {},
+	unpauseGame: () => {},
+});
 
 export default GameContext;
