1c3027ddf4c362059348fea3cf5e42856017462a
Michal Borowski <unpolacoo@gmail.com>
2020-08-28 01:33:45 +0200

    Updates

---
+++ precache-manifest.e679cd10783edab8e4f84245be232186.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58a3956643c6a93230ffd14f0bd421e8",
+    "url": "/warsawjs-workshop-47-reactive-snake/index.html"
+  },
+  {
+    "revision": "99b50b2c0470b5f57ed2",
+    "url": "/warsawjs-workshop-47-reactive-snake/static/css/main.b9a26f91.chunk.css"
+  },
+  {
+    "revision": "b68091109ceb56affa51",
+    "url": "/warsawjs-workshop-47-reactive-snake/static/js/2.feb86ac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/warsawjs-workshop-47-reactive-snake/static/js/2.feb86ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b50b2c0470b5f57ed2",
+    "url": "/warsawjs-workshop-47-reactive-snake/static/js/main.ad414dc5.chunk.js"
+  },
+  {
+    "revision": "87c0dc4a0a4af1b42347",
+    "url": "/warsawjs-workshop-47-reactive-snake/static/js/runtime-main.0f8dce98.js"
+  }
+]);
